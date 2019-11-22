9ceac11758cb2f4e20d146e4ab7eda1343a0b8f0
madmonl <madmonl@post.bgu.ac.il>
2019-11-22 23:26:59 +0200

    Traingle linting corrections

--- Triangle.java
+++ Triangle.java
@@ -1,28 +1,28 @@
 import java.util.Scanner;
-    public class Triangle
-{
- public static void main (String [] args)
- {
- Scanner scan = new Scanner (System.in);
-    System.out.println ("This program calculates the area "
-    + "and the perimeter of a given triangle. ");
-    System.out.println ("Please enter the three lengths"
+public class Triangle {
+  public static void main (String [] args) {
+    Scanner scan = new Scanner (System.in);
+    System.out.println("This program calculates the area "
+    + "and the perimeter of a given triangle.");
+    
+    System.out.println("Please enter the three lengths"
     + " of the triangle's sides");
+    
     int a = scan.nextInt();
     int b = scan.nextInt();
     int c = scan.nextInt();
-    int h = a+b+c; //היקף
-    int s = (h)/2 ; //חצי היקף
-         if (a>0 && b>0 && c>0){
-                    if (Math.abs(a-b)<c && c<(a+b)){
-                        System.out.println (h);
-                        System.out.println (Math.sqrt(s*((s-a)*(s-b)*(s-c))));
-                    }
-            else {
-                System.out.println ("ערכי צלעות לא תקינים");
-            }
-        }
-        else 
-                { System.out.println ("צלע משולש לא יכולה להיות שלילית");}
- } // end of method main
-} //end of class Triangle 
+    int e = a + b + c; // extent
+    int se = e / 2 ; // semi extent
+    
+    if (a <= 0 || b <= 0 || c <= 0) {
+      System.out.println("צלע משולש לא יכולה להיות שלילית או אפס");
+    }
+    
+    if ((Math.abs(a - b) < c) && (c < (a + b))) {
+      System.out.println(e);
+      System.out.println(Math.sqrt(se * ((se - a) * (se - b) * (se - c))));
+    } else {
+      System.out.println ("ערכי צלעות לא תקינים");
+    } 
+  }  
+} 
